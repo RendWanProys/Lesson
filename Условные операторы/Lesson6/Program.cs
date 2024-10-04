b16a2839895eb52f82e9c0c230131f1fc6da72a9
RendWanProys <122683762+RendWanProys@users.noreply.github.com>
2024-10-04 14:53:24 +0200

    Add files via upload

---
+++ Условные операторы/Lesson6/Program.cs
@@ -0,0 +1,18 @@
+﻿
+{
+    Console.WriteLine("Введите X");
+    int x = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите Y");
+    int y = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите радиус");
+    int R = Convert.ToInt32(Console.ReadLine());
+    double distance = Math.Sqrt(x * x + y * y);
+    if (distance <= R)
+    {
+        Console.WriteLine($"данная точка лежит внутри круга ({x},{y})");
+    }
+    else
+    {
+        Console.WriteLine($"данная точка не лежит внутри круга ({x},{y})");
+    }
+}
