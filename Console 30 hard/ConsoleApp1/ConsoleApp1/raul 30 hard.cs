394f19297e8f4455875f67cd5d04005f15ae029d
RendWanProys <122683762+RendWanProys@users.noreply.github.com>
2024-10-10 04:16:54 +0300

    Add files via upload

---
+++ Console 30 hard/ConsoleApp1/ConsoleApp1/raul 30 hard.cs
@@ -0,0 +1,12 @@
+﻿try{
+ Console.WriteLine("Введите прибыль за прошлый месяц:");
+    double profitLastMonth = double.Parse(Console.ReadLine());
+    Console.WriteLine("Введите себестоимость за прошлый месяц:");
+    double costLastMonth = double.Parse(Console.ReadLine());
+    double costThisMonth = costLastMonth * 0.95; // Уменьшение на 5%
+    double rent = (profitLastMonth / costThisMonth) * 100;
+    Console.WriteLine($"Рентабельность работы предприятия за месяц: {rent:F1}%");
+}
+catch{
+    
+}
