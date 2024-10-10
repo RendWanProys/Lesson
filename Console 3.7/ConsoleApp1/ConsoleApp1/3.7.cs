0270fa2b3301531d4576d33f07ff55167c1f6255
RendWanProys <122683762+RendWanProys@users.noreply.github.com>
2024-10-10 04:49:31 +0300

    Update 3.7.cs

--- Console 3.7/ConsoleApp1/ConsoleApp1/3.7.cs
+++ Console 3.7/ConsoleApp1/ConsoleApp1/3.7.cs
@@ -1,7 +1,7 @@
 ﻿try{
  Console.WriteLine("Введите номер квартиры: от 1 до 15");
 int n = int.Parse(Console.ReadLine());
-if (n>1&&n<15)
+if (n>=1&&n<=15)
 Console.WriteLine($"Этаж={(n-1)/3+1}");
 }
 catch
