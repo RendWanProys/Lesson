a8d25e34847e2a087987c49c6b66c25bd7d66b32
RendWanProys <122683762+RendWanProys@users.noreply.github.com>
2024-10-10 04:32:46 +0300

    Add files via upload

---
+++ Console 3.7/ConsoleApp1/ConsoleApp1/3.7.cs
@@ -0,0 +1,10 @@
+﻿try{
+ Console.WriteLine("Введите номер квартиры: от 1 до 15");
+int n = int.Parse(Console.ReadLine());
+if (n>1&&n<15)
+Console.WriteLine($"Этаж={(n-1)/3+1}");
+}
+catch
+{
+    Console.WriteLine("Введите данные правильно!");
+}
