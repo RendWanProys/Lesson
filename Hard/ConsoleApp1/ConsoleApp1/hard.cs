ddc2e4f9c4801c1a99ae808ae3917b8d9cef5688
RendWanProys <122683762+RendWanProys@users.noreply.github.com>
2024-10-10 18:51:06 +0300

    Add files via upload

---
+++ Hard/ConsoleApp1/ConsoleApp1/hard.cs
@@ -0,0 +1,14 @@
+﻿try{
+ Console.WriteLine("Введите x");
+ double x =double.Parse(Console.ReadLine());
+  Console.WriteLine("Введите y");
+ double y =double.Parse(Console.ReadLine());
+  Console.WriteLine("Введите t");
+ double t =double.Parse(Console.ReadLine());
+double P = (Math.Pow(Math.Sin(x), 3) + Math.Log(2 * y + 3 * x)) / (Math.Pow(t, Math.E) + Math.Sqrt(x));
+Console.WriteLine("P = "+P);
+}
+catch
+{
+    Console.WriteLine("Введите данные правильно!");
+}
