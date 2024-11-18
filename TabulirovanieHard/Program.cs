bcdef17cf8c01dfe77863d75aff22a84fc244f34
RendWanProys <122683762+RendWanProys@users.noreply.github.com>
2024-11-18 13:22:19 +0200

    Add files via upload

---
+++ TabulirovanieHard/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("┌---------------------------------------┐");
+Console.WriteLine($"|        x          |         y         |");
+Console.WriteLine("|---------------------------------------|");
+long factorial = 1;
+for (double x = -1; x <= 2; x += 0.3)
+{
+    double y = 0;
+    if (x > 0)
+    {
+        for (int k = 1; k <= 7; k++) y += Math.Pow(x, k) / (k!);
+    }
+    else y = Math.Atan(Math.Pow(double.Pi,x));
+    Console.WriteLine($"|       {x,-5:F2}       |       {y,5:F2}       |");
+}
+Console.WriteLine("└---------------------------------------┘");
+Console.WriteLine();
